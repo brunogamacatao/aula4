403a420ad17e8ea2c53094100f3ec79f283c9aaa
Aluno <aluno@cesed.local>
2017-03-02 19:16:30 -0300

    Primeiro commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+class Teste {
+    constructor(nome) {
+        this.nome = nome;
+    }
+
+    toString() {
+        return this.nome;
+    }
+}
+
+let t = new Teste("Fulano");
+console.log(t.toString());
